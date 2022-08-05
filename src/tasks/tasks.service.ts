66e40fdaa76c4ce40f76da12c1eade75e63587a3
Abdullah1428 <abdullahcse1428@gmail.com>
2022-08-05 12:25:25 +0200

    adding model and type safety for tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './tasks.model';
 
 @Injectable() // makes this singleton that can be shared across the application
 export class TasksService {
-  private tasks = [];
+  private tasks: Task[] = [];
 
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasks;
   }
 }
